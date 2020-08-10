6cace2be111d87a88205deb10baf371b7ac418e3
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2020-08-10 17:08:18 +0530

    Update host-setup.sh

--- host-setup.sh
+++ host-setup.sh
@@ -92,15 +92,28 @@ systemctl enable crio
 else
 
 # Setup for docker
-yum install -y docker
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  
+yum update -y && yum install -y containerd.io-1.2.13 docker-ce-19.03.11 docker-ce-cli-19.03.11
+  
+mkdir /etc/docker
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
 
-# Modify /etc/sysconfig/docker file as follows.
-more /etc/sysconfig/docker | grep OPTIONS
-sed -i "s/^OPTIONS=.*/OPTIONS='--selinux-enabled --signature-verification=false'/g" /etc/sysconfig/docker
-more /etc/sysconfig/docker | grep OPTIONS
-
-systemctl enable docker
-systemctl start docker
+mkdir -p /etc/systemd/system/docker.service.d
+systemctl daemon-reload; systemctl restart docker; systemctl enable docker
 
 cat <<EOF > /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
