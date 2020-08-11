e0f50f8018bd1375e47695953fe3dff2078146c0
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2020-08-11 06:27:19 +0530

    Update host-setup.sh

--- host-setup.sh
+++ host-setup.sh
@@ -92,28 +92,10 @@ systemctl enable crio
 else
 
 # Setup for docker
-yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-  
-yum update -y && yum install -y containerd.io-1.2.13 docker-ce-19.03.11 docker-ce-cli-19.03.11
-  
-mkdir /etc/docker
-cat > /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2",
-  "storage-opts": [
-    "overlay2.override_kernel_check=true"
-  ]
-}
-EOF
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 
-mkdir -p /etc/systemd/system/docker.service.d
-systemctl daemon-reload; systemctl restart docker; systemctl enable docker
+systemctl start docker; systemctl status docker; systemctl enable docker
 
 cat <<EOF > /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
