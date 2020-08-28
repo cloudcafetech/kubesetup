9c0c7ad795c90b61da459f299a31b1b65d7dbef8
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2020-08-28 09:15:03 +0530

    Update backup-setup.sh

--- misc/backup-setup.sh
+++ misc/backup-setup.sh
@@ -30,6 +30,9 @@ velero install \
     --cacert rootCA.pem \
     --snapshot-location-config region=minio
     
+kubectl patch deploy velero -n velero --type merge -p '{"spec": {"template": {"spec": {"nodeSelector": {"beta.kubernetes.io/os": "linux"}}}}}'
+kubectl patch ds restic -n velero --type merge -p '{"spec": {"template": {"spec": {"nodeSelector": {"beta.kubernetes.io/os": "linux"}}}}}'    
+    
 wget https://raw.githubusercontent.com/cloudcafetech/velero-backup-restore/master/velero-volume-controller.yaml    
 kubectl create -f velero-volume-controller.yaml    
 
