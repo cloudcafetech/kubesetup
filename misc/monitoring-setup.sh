9e99b25c952043d8e35e38725d5a383070f962d1
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2020-08-28 09:19:09 +0530

    Update monitoring-setup.sh

--- misc/monitoring-setup.sh
+++ misc/monitoring-setup.sh
@@ -43,9 +43,9 @@ echo ""
 echo "Waiting for Grafana POD ready to upload dashboard & loki datasource .."
 while [[ $(kubectl get pods kubemon-grafana-0 -n monitoring -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do printf '.'; sleep 2; done
 
-HIP=$(kubectl get svc kubemon-grafana -n monitoring | grep kubemon-grafana | awk '{print $4}')
-curl -vvv http://admin:admin2675@$HIP/api/dashboards/db -X POST -d @pod-monitoring.json -H 'Content-Type: application/json'
-curl -vvv http://admin:admin2675@$HIP/api/dashboards/db -X POST -d @kube-monitoring-overview.json -H 'Content-Type: application/json'
-curl -vvv http://admin:admin2675@$HIP/api/datasources -X POST -d @loki-ds.json -H 'Content-Type: application/json'
+HIP=`ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
+curl -vvv http://admin:admin2675@$HIP:30000/api/dashboards/db -X POST -d @pod-monitoring.json -H 'Content-Type: application/json'
+curl -vvv http://admin:admin2675@$HIP:30000/api/dashboards/db -X POST -d @kube-monitoring-overview.json -H 'Content-Type: application/json'
+curl -vvv http://admin:admin2675@$HIP:30000/api/datasources -X POST -d @loki-ds.json -H 'Content-Type: application/json' 
 
 kubectl get svc -n monitoring
