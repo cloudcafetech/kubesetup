80f2529de4516188f92f90e64f273ac7b7eb997a
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2020-09-18 15:57:07 +0530

    Update monitoring-setup.sh

--- misc/monitoring-setup.sh
+++ misc/monitoring-setup.sh
@@ -3,7 +3,7 @@
 
 wget https://raw.githubusercontent.com/cloudcafetech/kubesetup/master/logging/kubelog.yaml
 wget https://raw.githubusercontent.com/cloudcafetech/kubesetup/master/logging/loki.yaml
-wget https://raw.githubusercontent.com/cloudcafetech/kubesetup/master/logging/loki-ds.json
+#wget https://raw.githubusercontent.com/cloudcafetech/kubesetup/master/logging/loki-ds.json
 wget https://raw.githubusercontent.com/cloudcafetech/kubesetup/master/monitoring/kubemon.yaml
 wget https://raw.githubusercontent.com/cloudcafetech/kubesetup/master/monitoring/pod-monitoring.json
 wget https://raw.githubusercontent.com/cloudcafetech/kubesetup/master/monitoring/kube-monitoring-overview.json
@@ -17,11 +17,11 @@ kubectl create -f kubelog.yaml -n logging
 
 ## Upload Grafana dashboard & loki datasource
 echo ""
-echo "Waiting for Grafana POD ready to upload dashboard & loki datasource .."
+echo "Waiting for Grafana POD ready to upload dashboard .."
 while [[ $(kubectl get pods kubemon-grafana-0 -n monitoring -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do printf '.'; sleep 2; done
 
 HIP=`ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
 curl -vvv http://admin:admin2675@$HIP:30000/api/dashboards/db -X POST -d @pod-monitoring.json -H 'Content-Type: application/json'
 curl -vvv http://admin:admin2675@$HIP:30000/api/dashboards/db -X POST -d @kube-monitoring-overview.json -H 'Content-Type: application/json'
 curl -vvv http://admin:admin2675@$HIP:30000/api/dashboards/db -X POST -d @cluster-cost.json -H 'Content-Type: application/json'
-curl -vvv http://admin:admin2675@$HIP:30000/api/datasources -X POST -d @loki-ds.json -H 'Content-Type: application/json'
+#curl -vvv http://admin:admin2675@$HIP:30000/api/datasources -X POST -d @loki-ds.json -H 'Content-Type: application/json'
