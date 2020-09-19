d5b4502d8f004cc7bc5ef846dc75c45e22d91d87
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2020-09-19 08:48:58 +0530

    Update tracing-setup.sh

--- misc/tracing-setup.sh
+++ misc/tracing-setup.sh
@@ -11,6 +11,7 @@ wget https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/depl
 wget https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/cluster_role.yaml
 wget https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/cluster_role_binding.yaml
 
+sed -i '0,/fieldP/{//d;}' operator.yaml; sed -i '0,/fieldR/{//d;}' operator.yaml; sed -i '0,/valueFrom:/{s/valueFrom:/value: ""/}' operator.yaml
 sed -i "s/observability/tracing/g" cluster_role_binding.yaml
 
 kubectl create -f jaegertracing.io_jaegers_crd.yaml -f service_account.yaml -f role.yaml -f role_binding.yaml -f operator.yaml -n tracing
