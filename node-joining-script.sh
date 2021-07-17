fd6356e06b2aab43f9b1fd9668a138f647781710
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2021-07-17 12:58:02 +0530

    Update node-joining-script.sh

--- node-joining-script.sh
+++ node-joining-script.sh
@@ -2,28 +2,29 @@
 # Controlpane & node joining script
 # Note: If certificate-key expire, generate new using (kubeadm init phase upload-certs --upload-certs)
 
-HA_PROXY_LB_DNS=172.31.28.205
+HA_PROXY_LB_DNS=172.31.28.212
 HA_PROXY_LB_PORT=6443
-MASTER1_IP=172.31.26.135
-MASTER2_IP=172.31.21.124
-MASTER3_IP=172.31.31.51
+MASTER1_IP=172.31.17.140
+MASTER2_IP=172.31.21.8
+MASTER3_IP=172.31.30.167
 NODE1=172.31.22.99
 DATE=$(date +"%d%m%y")
 TOKEN=$DATE.1a7dd4cc8d1f4cc5
 CERTKEY=d60a03f140d7f245c06879ac6ab22aa3408b85a60edb94917d67add3dc2a5fa7
 
-joinMaster1="sudo kubeadm init --token=$TOKEN --control-plane-endpoint "$HA_PROXY_LB_DNS:$HA_PROXY_LB_PORT" --upload-certs --certificate-key=$CERTKEY --pod-network-cidr=192.168.0.0/16 --kubernetes-version $(kubeadm version -o short) --ignore-preflight-errors=all | tee kubeadm-output.txt"
-joinMaster="sudo kubeadm join $HA_PROXY_LB_DNS:$HA_PROXY_LB_PORT --token=$TOKEN --control-plane --certificate-key=$CERTKEY --discovery-token-ca-cert-hash sha256:$tokenSHA --ignore-preflight-errors=all"
-joinNode="sudo kubeadm join $HA_PROXY_LB_DNS:$HA_PROXY_LB_PORT --token=$TOKEN --discovery-token-ca-cert-hash sha256:$tokenSHA --ignore-preflight-errors=all"
-
-
 # Host Preparation
 for hip in $MASTER1_IP $MASTER2_IP $MASTER3_IP $NODE1
 do
 echo "K8S Host Preparation on $hip"
-scp ec2-user@$hip -i key.pem k8s-host-setup.sh ec2-user@$nip:/home/ec2-user/
-ssh ec2-user@$hip -i key.pem /home/ec2-user/k8s-host-setup.sh
+#scp ec2-user@$hip -i key.pem ./k8s-host-setup.sh ec2-user@$hip:/home/ec2-user/k8s-host-setup.sh
+ssh ec2-user@$hip -i key.pem "wget https://raw.githubusercontent.com/cloudcafetech/kubesetup/master/k8s-host-setup.sh"
+ssh ec2-user@$hip -i key.pem "chmod +x /home/ec2-user/k8s-host-setup.sh"
+ssh ec2-user@$hip -i key.pem "/home/ec2-user/k8s-host-setup.sh"
 done
+
+joinMaster1="sudo kubeadm init --token=$TOKEN --control-plane-endpoint "$HA_PROXY_LB_DNS:$HA_PROXY_LB_PORT" --upload-certs --certificate-key=$CERTKEY --pod-network-cidr=192.168.0.0/16 --kubernetes-version $(kubeadm version -o short) --ignore-preflight-errors=all | tee kubeadm-output.txt"
+joinMaster="sudo kubeadm join $HA_PROXY_LB_DNS:$HA_PROXY_LB_PORT --token=$TOKEN --control-plane --certificate-key=$CERTKEY --discovery-token-ca-cert-hash sha256:$tokenSHA --ignore-preflight-errors=all"
+joinNode="sudo kubeadm join $HA_PROXY_LB_DNS:$HA_PROXY_LB_PORT --token=$TOKEN --discovery-token-ca-cert-hash sha256:$tokenSHA --ignore-preflight-errors=all"
 
 # Setup for Master 1
 echo "Initialize Masters #1"
