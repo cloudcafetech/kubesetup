813c28a8115c1cefd349a64af55d0f7e60f79cf3
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2021-07-18 09:35:55 +0530

    Update node-joining-script.sh

--- node-joining-script.sh
+++ node-joining-script.sh
@@ -8,6 +8,10 @@ MASTER1_IP=172.31.17.140
 MASTER2_IP=172.31.21.8
 MASTER3_IP=172.31.30.167
 NODE1=172.31.22.99
+
+USER=ec2-user
+PEMKEY=key.pem
+
 DATE=$(date +"%d%m%y")
 TOKEN=$DATE.1a7dd4cc8d1f4cc5
 CERTKEY=d60a03f140d7f245c06879ac6ab22aa3408b85a60edb94917d67add3dc2a5fa7
@@ -39,38 +43,38 @@ done
 for hip in $MASTER1_IP $MASTER2_IP $MASTER3_IP $NODE1
 do
 echo "K8S Host Preparation on $hip"
-ssh ec2-user@$hip -o 'StrictHostKeyChecking no' -i key.pem "wget https://raw.githubusercontent.com/cloudcafetech/kubesetup/master/k8s-host-setup.sh"
-ssh ec2-user@$hip -o 'StrictHostKeyChecking no' -i key.pem "chmod +x /home/ec2-user/k8s-host-setup.sh"
-ssh ec2-user@$hip -o 'StrictHostKeyChecking no' -i key.pem "/home/ec2-user/k8s-host-setup.sh"
+ssh $USER@$hip -o 'StrictHostKeyChecking no' -i $PEMKEY "wget https://raw.githubusercontent.com/cloudcafetech/kubesetup/master/k8s-host-setup.sh"
+ssh $USER@$hip -o 'StrictHostKeyChecking no' -i $PEMKEY "chmod +x /home/$USER/k8s-host-setup.sh"
+ssh $USER@$hip -o 'StrictHostKeyChecking no' -i $PEMKEY "/home/$USER/k8s-host-setup.sh"
 done
 
 # Setup for Master 1
 echo "Initialize Masters #1"
 joinMaster1="sudo kubeadm init --token=$TOKEN --control-plane-endpoint $HA_PROXY_LB_DNS:$HA_PROXY_LB_PORT --pod-network-cidr=192.168.0.0/16 --upload-certs --certificate-key=$CERTKEY --ignore-preflight-errors=all | tee kubeadm-output.txt"
 #joinMaster1="sudo kubeadm init --token=$TOKEN --control-plane-endpoint $HA_PROXY_LB_DNS:$HA_PROXY_LB_PORT --upload-certs --certificate-key=$CERTKEY --ignore-preflight-errors=all | tee kubeadm-output.txt"
-ssh ec2-user@$MASTER1_IP -o 'StrictHostKeyChecking no' -i key.pem $joinMaster1
-ssh ec2-user@$MASTER1_IP -o 'StrictHostKeyChecking no' -i key.pem "sudo cp /etc/kubernetes/admin.conf /home/ec2-user/config"
-ssh ec2-user@$MASTER1_IP -o 'StrictHostKeyChecking no' -i key.pem "sudo chown ec2-user:ec2-user /home/ec2-user/config"
+ssh $USER@$MASTER1_IP -o 'StrictHostKeyChecking no' -i $PEMKEY $joinMaster1
+ssh $USER@$MASTER1_IP -o 'StrictHostKeyChecking no' -i $PEMKEY "sudo cp /etc/kubernetes/admin.conf /home/$USER/config"
+ssh $USER@$MASTER1_IP -o 'StrictHostKeyChecking no' -i $PEMKEY "sudo chown $USER:$USER /home/$USER/config"
 mkdir -p $HOME/.kube
-scp -o 'StrictHostKeyChecking no' -i key.pem ec2-user@$MASTER1_IP:/home/ec2-user/config $HOME/.kube/
+scp -o 'StrictHostKeyChecking no' -i $PEMKEY $USER@$MASTER1_IP:/home/$USER/config $HOME/.kube/
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-tokenSHA=$(ssh ec2-user@$MASTER1_IP -o 'StrictHostKeyChecking no' -i key.pem "sudo openssl x509 -in /etc/kubernetes/pki/ca.crt -noout -pubkey | sudo openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | cut -d' ' -f1")
+tokenSHA=$(ssh $USER@$MASTER1_IP -o 'StrictHostKeyChecking no' -i $PEMKEY "sudo openssl x509 -in /etc/kubernetes/pki/ca.crt -noout -pubkey | sudo openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | cut -d' ' -f1")
 joinMaster="sudo kubeadm join $HA_PROXY_LB_DNS:$HA_PROXY_LB_PORT --token=$TOKEN --control-plane --certificate-key=$CERTKEY --discovery-token-ca-cert-hash sha256:$tokenSHA --ignore-preflight-errors=all"
 joinNode="sudo kubeadm join $HA_PROXY_LB_DNS:$HA_PROXY_LB_PORT --token=$TOKEN --discovery-token-ca-cert-hash sha256:$tokenSHA --ignore-preflight-errors=all"
 # Setup for Master 2
 echo "Joining Masters #2"
-ssh ec2-user@$MASTER2_IP -o 'StrictHostKeyChecking no' -i key.pem $joinMaster
+ssh $USER@$MASTER2_IP -o 'StrictHostKeyChecking no' -i $PEMKEY $joinMaster
 
 # Setup for Master 3
 echo "Joining Masters #3"
-ssh ec2-user@$MASTER3_IP -o 'StrictHostKeyChecking no' -i key.pem $joinMaster
+ssh $USER@$MASTER3_IP -o 'StrictHostKeyChecking no' -i $PEMKEY $joinMaster
 
 # Node
 for nip in $NODE1
 do
 echo "Joining Nodes"
-ssh ec2-user@$nip -o 'StrictHostKeyChecking no' -i key.pem $joinNode
+ssh $USER@$nip -o 'StrictHostKeyChecking no' -i $PEMKEY $joinNode
 done
 
 # Setup K8S Networking using Calico
@@ -82,4 +86,3 @@ kubectl apply -f calico.yaml
 #kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
 #kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 #kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
