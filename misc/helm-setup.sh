ed03911084c2c782ea52129126e539fc23a340a2
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2022-03-19 06:12:49 +0530

    Update helm-setup.sh

--- misc/helm-setup.sh
+++ misc/helm-setup.sh
@@ -2,9 +2,6 @@
 # Install helm in Kubernetes
 
 cd /tmp
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > install-helm.sh
-chmod u+x install-helm.sh
-/tmp/install-helm.sh
-kubectl -n kube-system create sa tiller
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
