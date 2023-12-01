a97f446d636cc6b671343a3aec9ce7388aeb8329
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2023-12-01 12:36:00 +0530

    Update host-setup.sh

--- host-setup.sh
+++ host-setup.sh
@@ -191,11 +191,12 @@ chmod +x ./kafka-setup.sh
 
 # Install krew
 set -x; cd "$(mktemp -d)" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
-  tar zxvf krew.tar.gz &&
-  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
-  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
-  "$KREW" update
+OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+KREW="krew-${OS}_${ARCH}" &&
+curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+tar zxvf "${KREW}.tar.gz" &&
+./"${KREW}" install krew
   
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
